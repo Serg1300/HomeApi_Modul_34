199b40978dcc5ec90b54d8c7cad989cbd6163f7d
Serg1300 <kul3@yandex.ru>
2023-12-20 22:39:15 +0300

    Добавьте файлы проекта.

---
+++ HomeApi.Contracts/Validation/EditRoomRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using HomeApi.Contracts.Models.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApi.Contracts.Validation
+{
+    public class EditRoomRequestValidator : AbstractValidator<EditRoomRequest>
+    {
+        public EditRoomRequestValidator() 
+        {
+            RuleFor(x => x.NewName).NotEmpty();                 
+            RuleFor(x => x.NewArea).NotEmpty();
+            RuleFor(x => x.NewGasConnected).NotEmpty();
+            RuleFor(x => x.NewVoltage).NotEmpty();
+        }
+        
+    }
+}
---
+++ HomeApi.Data/Repos/RoomRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeApi.Data.Models;
+using HomeApi.Data.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeApi.Data.Repos
+{
+    /// <summary>
+    /// Репозиторий для операций с объектами типа "Room" в базе
+    /// </summary>
+    public class RoomRepository : IRoomRepository
+    {
+        private readonly HomeApiContext _context;
+
+        public RoomRepository(HomeApiContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        ///  Найти комнату по имени
+        /// </summary>
+        public async Task<Room> GetRoomByName(string name)
+        {
+            return await _context.Rooms.Where(r => r.Name == name).FirstOrDefaultAsync();
+        }
+
+        /// <summary>
+        ///  Добавить новую комнату
+        /// </summary>
+        public async Task AddRoom(Room room)
+        {
+            var entry = _context.Entry(room);
+            if (entry.State == EntityState.Detached)
+                await _context.Rooms.AddAsync(room);
+
+            await _context.SaveChangesAsync();
+        }
+        /// <summary>
+        /// Выгрузить все комнаты
+        /// </summary>        
+        public async Task<Room[]> GetRooms()
+        {
+            return await _context.Rooms.ToArrayAsync();
+        }
+
+        public async Task<Room> GetRoomById(Guid id)
+        {
+            return await _context.Rooms.Where(d => d.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task UpdateRoom(Room room, UpdateRoomQuery query)
+        {
+            if (!string.IsNullOrEmpty(query.NewName))
+                room.Name = query.NewName;
+            if (query.NewArea != 0)
+                room.Area = query.NewArea;
+            if (query.NewGasConnected)
+                room.GasConnected = query.NewGasConnected;
+            if (query.NewVoltage != 0)
+                room.Voltage = query.NewVoltage;
+
+            var entry = _context.Entry(room);
+            if(entry.State == EntityState.Detached)
+                _context.Rooms.Update(room);
+            await _context.SaveChangesAsync();
+
+        }
+    }
+}
